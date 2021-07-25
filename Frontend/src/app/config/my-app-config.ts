f93bd7fb0b385c8920d16f12c7752a903635a417
L.S.M. De Silva <54409616+Code2Resolve@users.noreply.github.com>
2021-07-26 00:19:50 +0530

    Add files via upload

---
+++ Frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa19q1e22Sy62qK55d7',
+        issuer: 'https://dev-53204158.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
